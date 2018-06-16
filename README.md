7564a1a0982aeb2ea76924c19cd3e003ecdd860b
afeiship <1290657123@qq.com>
2018-06-16 15:51:05 +0800

    issue: update

---
+++ README.md
@@ -0,0 +1,12 @@
+# next-map-value
+> Map value for next
+
+## install:
+```bash
+npm install -S afeiship/next-map-value --registry=https://registry.npm.taobao.org
+```
+
+## usage:
+```js
+//DOCS here!
+```
---
+++ dist/next-map-value.js
@@ -0,0 +1,20 @@
+(function () {
+
+  var global = global || this || self || window;
+  var nx = global.nx || require('next-js-core2');
+
+  nx.mapValue = function (inObj, inCallback, inContext) {
+    var result = {};
+    nx.each(inObj, function (key, value) {
+      var _value = inCallback.call(inContext, key, value, inObj);
+      result[key] = _value;
+    }, inContext);
+    return result;
+  };
+
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = nx.mapValue;
+  }
+
+}());
