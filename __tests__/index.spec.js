4b424d8dfb0e7b2ea22fff5d85054c2acd0ead6b
afeiship <1290657123@qq.com>
2021-08-07 09:15:47 +0800

    test: update spec other solution

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,6 +1,16 @@
 (function () {
   require('../src');
 
+  /*
+  // other solution:
+  // @url: https://masteringjs.io/tutorials/fundamentals/filter-object
+
+  function filterObject(obj, callback) {
+    return Object.fromEntries(Object.entries(obj).
+      filter(([key, val]) => callback(val, key)));
+  }
+  */
+
   describe('api.basic test', () => {
     test('nx.objectFilter should filter object', function () {
       const env = process.env;
@@ -8,7 +18,7 @@
         return key.includes('npm_config');
       });
       const keys = Object.keys(res);
-      const allTruthy = keys.every(key => key.includes('npm_config'));
+      const allTruthy = keys.every((key) => key.includes('npm_config'));
       expect(allTruthy).toBe(true);
     });
   });
